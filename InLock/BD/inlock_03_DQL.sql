f7e466c8203abd8e51b614311cb983371547dbfa
Pedro Alves <pedro22carro@gmail.com>
2021-04-22 00:37:22 -0300

    inlock

---
+++ InLock/BD/inlock_03_DQL.sql
@@ -0,0 +1,35 @@
+USE inlock_games_manha;
+GO
+
+SELECT * FROM usuarios;
+GO
+
+SELECT * FROM estudios;
+GO
+
+SELECT * FROM jogos;
+GO
+
+SELECT jogos.nomeJogo AS Jogo, estudios.nomeEstudio AS Estudio
+FROM jogos
+LEFT JOIN estudios
+ON jogos.idEstudio = estudios.idEstudio;
+GO
+
+SELECT estudios.nomeEstudio AS Estudio, jogos.nomeJogo AS Jogo
+FROM jogos
+RIGHT JOIN estudios
+ON jogos.idEstudio = estudios.idEstudio;
+GO
+
+SELECT * FROM jogos
+WHERE idJogo = '1';
+GO
+
+SELECT * FROM estudios
+WHERE idEstudio = '1';
+GO
+
+SELECT * FROM usuarios
+WHERE email = 'admin@admin.com' AND senha = 'admin';
+GO
